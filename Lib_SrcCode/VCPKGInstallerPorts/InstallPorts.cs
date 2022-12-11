60950ca115fe7e0d0cc9fcf50d77c3910df590d9
RyuzakiEverywhere aka LLawlietEverywhere <119974711+RyuzakiEverywhere@users.noreply.github.com>
2022-12-11 22:09:26 +0200

    Add files via upload

---
+++ Lib_SrcCode/VCPKGInstallerPorts/InstallPorts.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VCPKGInstallerPorts
+{
+    public class InstallPorts
+    {
+        public void InstallViaString(string ports_name, string x64_or_x86, string os_name)
+        {
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.FileName = "cmd.exe";
+            start.Arguments = $"/c vcpkg install {ports_name} {x64_or_x86}:{os_name}";
+            start.Verb = "runas";
+            start.WindowStyle = ProcessWindowStyle.Normal;
+            Process.Start(start);
+        }
+        public bool DetectVCPKG()
+        {
+            if (Directory.Exists("C:\\vcpkg"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
